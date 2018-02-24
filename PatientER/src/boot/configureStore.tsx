98876d5931534f0e351eccdac6501b78df6cdf22
Mitch Pynn <mitchell.pynn@gmail.com>
2018-02-23 20:05:28 -0500

    Config'd SurveyStore

--- PatientER/src/boot/configureStore.tsx
+++ PatientER/src/boot/configureStore.tsx
@@ -1,15 +1,18 @@
 import MainStore from "../store/DomainStore/HomeStore";
 import LoginStore from "../store/ViewStore/LoginViewStore";
 import TwoFactor from "../store/TwoFactorStore/TwoFactorStore";
+import SurveyStore from "../store/SurveyStore/SurveyStore";
 
 export default function() {
   const mainStore = new MainStore();
   const loginForm = new LoginStore();
   const twoFactor = new TwoFactor();
+  const surveyStore = new SurveyStore();
 
   return {
     loginForm,
     mainStore,
-    twoFactor
+    twoFactor,
+    surveyStore
   };
 }
