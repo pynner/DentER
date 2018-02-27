a2568270ee4dd043239f5e7892c98c86bc50189a
Kyle Robinson <kyle709@gmail.com>
2018-02-26 20:54:12 -0500

    Integrated AWS App Hub connectivity for future authentication.

--- Client/App.js
+++ Client/App.js
@@ -1,5 +1,10 @@
 import boot from "./artifacts/boot/index";
+import Amplify from 'aws-amplify';
+import aws_exports from './src/aws-exports';
+import { withAuthenticator } from 'aws-amplify-react-native';
+
+Amplify.configure(aws_exports)
 
 const app = boot();
 
-export default app;
+export default withAuthenticator(app);
