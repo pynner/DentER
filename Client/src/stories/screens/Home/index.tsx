7b9b83d62dcc5f380e992050d16b4ee318cfe8bb
Mitch Pynn <mitchell.pynn@gmail.com>
2018-03-24 04:12:13 -0400

    Fixed jeremy's branch so it actually works
    - Sidebar fixed
    - Home page now has import
    - Spacing fixed for styles

--- Client/src/stories/screens/Home/index.tsx
+++ Client/src/stories/screens/Home/index.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Image, Platform } from "react-native";
+import { Image } from "react-native";
 import {
   Container,
   Header,
@@ -10,10 +10,10 @@ import {
   Left,
   Body,
   Right,
-  H1,
   H3,
   Card,
-  CardItem
+  CardItem,
+  Text
 } from "native-base";
 
 import styles from "./styles";
@@ -22,55 +22,51 @@ export interface Props {
 }
 export interface State {}
 class Home extends React.Component<Props, State> {
-	render() {
-		return (
-			<Container style={styles.container}>
-				<Header>
-					<Left>
-						<Button transparent>
-							<Icon
-								active
-								name="menu"
-								onPress={() => this.props.navigation.navigate("DrawerOpen")}
-							/>
-					</Button>
-					</Left>
-					<Body>
-						<Title>Home</Title>
-					</Body>
-					<Right />
-				</Header>
-				<Content padder>
-					<Body>
-						<Image
-							source={require("../../../../assets/DentER_logo.png")}
-							style={{ width: 650 / 4, height: 650 / 4 }}
-						/>
-					</Body>
-					<Card>
-						<CardItem header>
-							<Body>
-								<H3>Welcome to DentER!</H3>
-							</Body>
-						</CardItem>
-						<CardItem>
-							<Body>
-								<Text>
-									The best way to connect with on-call dentists.
-								</Text>
-							</Body>
-						</CardItem>
-					</Card>
-					<Body>
-						<Text>   </Text>
-						<Text>
-							Swipe right to get started.
-						</Text>
-					</Body>
-				</Content>
-			</Container>
-		);
-	}
+  render() {
+    return (
+      <Container style={styles.container}>
+        <Header>
+          <Left>
+            <Button transparent>
+              <Icon
+                active
+                name="menu"
+                onPress={() => this.props.navigation.navigate("DrawerOpen")}
+              />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Home</Title>
+          </Body>
+          <Right />
+        </Header>
+        <Content padder>
+          <Body>
+            <Image
+              source={require("../../../../assets/DentER_logo.png")}
+              style={{ width: 650 / 4, height: 650 / 4 }}
+            />
+          </Body>
+          <Card>
+            <CardItem header>
+              <Body>
+                <H3>Welcome to DentER!</H3>
+              </Body>
+            </CardItem>
+            <CardItem>
+              <Body>
+                <Text>The best way to connect with on-call dentists.</Text>
+              </Body>
+            </CardItem>
+          </Card>
+          <Body>
+            <Text> </Text>
+            <Text>Swipe right to get started.</Text>
+          </Body>
+        </Content>
+      </Container>
+    );
+  }
 }
 
 export default Home;
--- Client/src/stories/screens/Sidebar/index.tsx
+++ Client/src/stories/screens/Sidebar/index.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { Image, Platform } from "react-native";
 import {
   Text,
   Container,
@@ -10,12 +9,12 @@ import {
   Button,
   Body,
   FooterTab,
-  H1, H2, H3,
+  H2,
+  H3,
   CardItem,
-  Left, Right,
+  Left,
   Thumbnail
 } from "native-base";
-import { NavigationActions } from "react-navigation";
 
 import { Auth } from "aws-amplify";
 
@@ -28,71 +27,73 @@ export default class Sidebar extends React.Component<Props, State> {
   render() {
     return (
       <Container>
-        <Content>
-          <CardItem>
-            <Left>
-              <Thumbnail source={require("../../../../assets/profile.jpg")} />
-              <Body>
-                <Text>Mitchell Pynn</Text>
-                <Text note>Patient</Text>
-              </Body>
-            </Left>
-          </CardItem>
-          <Button full info onPress={() => Auth.signOut()
-                  .then(data => console.log(data))
-                  .catch(err => console.log(err));}>
-            <Text>Sign out</Text>
-          </Button>
-          <List style={{ marginTop: 0 }}>
+        <CardItem>
+          <Left>
+            <Thumbnail source={require("../../../../assets/profile.jpg")} />
+            <Body>
+              <Text>Mitchell Pynn</Text>
+              <Text note>Patient</Text>
+            </Body>
+          </Left>
+        </CardItem>
 
-            
+        <Button
+          full
+          info
+          onPress={() =>
+            Auth.signOut()
+              .then(data => console.log(data))
+              .catch(err => console.log(err))
+          }
+        >
+          <Text>Sign out</Text>
+        </Button>
 
-            <ListItem>
-              <H3>Patient</H3>
-            </ListItem>
-            
-            <ListItem
-              button
-              onPress={() => {
-                this.props.navigation.navigate("Home");
-              }}
-            >
-              <Text>Home</Text>
-            </ListItem>
+        <List style={{ marginTop: 0 }}>
+          <ListItem>
+            <H3>Patient</H3>
+          </ListItem>
 
-            <ListItem
-              button
-              onPress={() => {
-                this.props.navigation.navigate("Calender");
-              }}
-            >
-              <Text>Calendar</Text>
-            </ListItem>
-            <ListItem>
-              <H3>Dentist</H3>
-            </ListItem>
+          <ListItem
+            button
+            onPress={() => {
+              this.props.navigation.navigate("Home");
+            }}
+          >
+            <Text>Home</Text>
+          </ListItem>
 
-            <ListItem
-              button
-              onPress={() => {
-                this.props.navigation.navigate("Home");
+          <ListItem
+            button
+            onPress={() => {
+              this.props.navigation.navigate("Calender");
+            }}
+          >
+            <Text>Calendar</Text>
+          </ListItem>
+          <ListItem>
+            <H3>Dentist</H3>
+          </ListItem>
 
-              }}
-            >
-              <Text>Home</Text>
-            </ListItem>
+          <ListItem
+            button
+            onPress={() => {
+              this.props.navigation.navigate("Home");
+            }}
+          >
+            <Text>Home</Text>
+          </ListItem>
 
-            <ListItem
-              button
-              onPress={() => {
-                this.props.navigation.navigate("Calender");
-              }}
-            >
-              <Text>Calendar</Text>
-            </ListItem>
-
-          </List>
-        </Content>
+          <ListItem
+            button
+            onPress={() => {
+              this.props.navigation.navigate("Calender");
+            }}
+          >
+            <Text>Calendar</Text>
+          </ListItem>
+        </List>
+        <Content />
 
         <Footer>
           <FooterTab>
