5fd975b320e296b42aa575c5a0c0f7874eacf512
Mitch Pynn <mitchell.pynn@gmail.com>
2018-03-25 15:11:43 -0400

    Get current user to request for dentist submissions

--- Client/src/store/SubmissionsStore/SubmissionsStore.tsx
+++ Client/src/store/SubmissionsStore/SubmissionsStore.tsx
@@ -1,22 +1,38 @@
 import { observable, action } from "mobx";
 
-import { API } from "aws-amplify";
+import { API, Auth } from "aws-amplify";
 
 class SubmissionsStore {
+  @observable submissionsArray = [];
+
   @action
   async getAllSubmissions() {
-    // const path = "/survey/pynner1521863441110";
-    // const path = "/survey/allObj/test";
-    const path = "/survey/dentist/Dr.Lameo";
+    const currentUser = await Auth.currentAuthenticatedUser()
+      .then(response => {
+        return response.username;
+      })
+      .catch(error => {
+        console.log("Error getting user:" + error);
+      });
+    const path = "/survey/dentist/Dr." + currentUser;
+    console.log(currentUser);
     const myInit = {
       headers: {}
     };
     console.log("Hellllo");
 
     try {
-      const apiResponse = await API.get("surveyCRUD", path, myInit);
+      this.submissionsArray = await API.get("surveyCRUD", path, myInit).then(
+        response => {
+          return response.data;
+        }
+      );
       console.log("response from getting surveys: ");
-      console.log(apiResponse);
+      // console.log(apiResponse);
+      // this.submissionsArray = apiResponse;
+      console.log(this.submissionsArray);
+      console.log(this.submissionsArray.length);
+      console.log(this.submissionsArray[0].dentist);
     } catch (e) {
       console.log(e);
     }
