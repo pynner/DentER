551b8c62551f263cfc773a8a20866804d9151326
Mitch Pynn <mitchell.pynn@gmail.com>
2018-03-25 17:54:10 -0400

    Pulling data from server for submissions

--- Client/src/stories/screens/SubmissionDetails/index.tsx
+++ Client/src/stories/screens/SubmissionDetails/index.tsx
@@ -12,7 +12,8 @@ import {
   Card,
   CardItem,
   Text,
-  Separator, H3
+  Separator,
+  H3
 } from "native-base";
 import { NavigationActions } from "react-navigation";
 
@@ -28,6 +29,7 @@ const resetAction = NavigationActions.reset({
 export interface State {}
 class SubmissionDetails extends React.Component<Props, State> {
   render() {
+    const param = this.props.navigation.state.params;
     return (
       <Container style={styles.container}>
         <Header>
@@ -51,68 +53,68 @@ class SubmissionDetails extends React.Component<Props, State> {
           </Right>
         </Header>
         <Content padder>
-        <Card>
-          <CardItem header>
-            <H3>Susie Green</H3>
-          </CardItem>
-          <Separator bordered>
-            <Text>PAIN DESCRIPTION</Text>
-          </Separator>
-          <CardItem>
-            <Body>
-              <Text>Where?</Text>
-            </Body>
-            <Right>
-              <Text note>Gums</Text>
-            </Right>
-          </CardItem>
-          <CardItem>
-            <Body>
-              <Text>How long?</Text>
-            </Body>
-            <Right>
-              <Text note>A few days</Text>
-            </Right>
-          </CardItem>
-          <Separator bordered>
-            <Text>COMMON SYMPTOMS</Text>
-          </Separator>
-          <CardItem>
-            <Body>
-              <Text>Bleeding</Text>
-            </Body>
-            <Right>
-              <Text note>Yes</Text>
-            </Right>
-          </CardItem>
-          <CardItem>
-            <Body>
-              <Text>Hard Lumps</Text>
-            </Body>
-            <Right>
-              <Text note>No</Text>
-            </Right>
-          </CardItem>
-          <CardItem>
-            <Body>
-              <Text>Soft Lumps</Text>
-            </Body>
-            <Right>
-              <Text note>No</Text>
-            </Right>
-          </CardItem>
-          <Separator bordered>
-            <Text>ADDITIONAL COMMENTS</Text>
-          </Separator>
-          <CardItem>
-              <Text style={{color: "gray"}}>
-                The pain is so bad I cannot eat.
-                I am bedridden, and my children are going hungry because
-                they are too young to feed themselves. My many cats are also
-                starting to feel the effects of hunger, and I worry for their lives.
+          <Card>
+            <CardItem header>
+              <H3>Susie Green</H3>
+            </CardItem>
+            <Separator bordered>
+              <Text>PAIN DESCRIPTION</Text>
+            </Separator>
+            <CardItem>
+              <Body>
+                <Text>Where?</Text>
+              </Body>
+              <Right>
+                <Text note>Gums</Text>
+              </Right>
+            </CardItem>
+            <CardItem>
+              <Body>
+                <Text>How long?</Text>
+              </Body>
+              <Right>
+                <Text note>A few days</Text>
+              </Right>
+            </CardItem>
+            <Separator bordered>
+              <Text>COMMON SYMPTOMS</Text>
+            </Separator>
+            <CardItem>
+              <Body>
+                <Text>Bleeding</Text>
+              </Body>
+              <Right>
+                <Text note>Yes</Text>
+              </Right>
+            </CardItem>
+            <CardItem>
+              <Body>
+                <Text>Hard Lumps</Text>
+              </Body>
+              <Right>
+                <Text note>No</Text>
+              </Right>
+            </CardItem>
+            <CardItem>
+              <Body>
+                <Text>Soft Lumps</Text>
+              </Body>
+              <Right>
+                <Text note>No</Text>
+              </Right>
+            </CardItem>
+            <Separator bordered>
+              <Text>ADDITIONAL COMMENTS</Text>
+            </Separator>
+            <CardItem>
+              <Text style={{ color: "gray" }}>
+                The pain is so bad I cannot eat. I am bedridden, and my children
+                are going hungry because they are too young to feed themselves.
+                My many cats are also starting to feel the effects of hunger,
+                and I worry for their lives.
               </Text>
-          </CardItem>
-        </Card>
+            </CardItem>
+          </Card>
         </Content>
       </Container>
     );
