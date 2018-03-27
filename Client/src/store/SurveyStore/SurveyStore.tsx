ecb900a5bc332c71cc069749fb49636ca94de478
Mitch Pynn <mitchell.pynn@gmail.com>
2018-03-27 04:04:08 -0400

    Text input limits, added icons and splash

--- Client/src/store/SurveyStore/SurveyStore.tsx
+++ Client/src/store/SurveyStore/SurveyStore.tsx
@@ -94,7 +94,7 @@ class SurveyStore {
       });
     // @TODO get dentist from DB, make sure call to get current dentist with current time
 
-    const path = "/calender/getdentist/1522102295490";
+    const path = "/calender/getdentist/" + new Date().getTime();
 
     const myInit = {
       headers: {}
